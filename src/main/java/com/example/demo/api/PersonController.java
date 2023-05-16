1ced78ec01b548a5acbe55544a2f83ce2870185a
VicEmmanuel <vicemmanuel7@gmail.com>
2023-05-16 09:54:40 +0100

    Progress made on person api

--- src/main/java/com/example/demo/api/PersonController.java
+++ src/main/java/com/example/demo/api/PersonController.java
@@ -2,15 +2,45 @@ package com.example.demo.api;
 
 import com.example.demo.model.Person;
 import com.example.demo.service.PersonService;
+import com.sun.istack.internal.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.UUID;
+
+@RequestMapping("api/v1/person")
+@RestController
 public class PersonController {
     private final PersonService personService;
 
+    @Autowired
     public PersonController(PersonService personService) {
         this.personService = personService;
     }
 
-    public void addPerson(Person person){
+    @PostMapping
+    public void addPerson(@NotNull @RequestBody Person person){
         personService.addPerson(person);
+    }
+
+    @GetMapping
+    public List<Person> getAllPeople(){
+        return  personService.getAllPeople();
+    }
+
+    @GetMapping(path = "/{id}")
+    public  Person getPersonById(@PathVariable("id") UUID id){
+        return  personService.getPersonById(id).orElse(null);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public void deletePersonById(@PathVariable("id") UUID id){
+        personService.deletePersonById(id);
+    }
+
+    @PutMapping(path = "/{id}")
+    public  void updatePersonById(@PathVariable("id") UUID id, @NotNull @RequestBody Person personToUpdate){
+        personService.updatePerson(id, personToUpdate);
     }
 }
--- src/main/java/com/example/demo/model/Person.java
+++ src/main/java/com/example/demo/model/Person.java
@@ -1,12 +1,18 @@
 package com.example.demo.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.sun.istack.internal.NotNull;
+
 import java.util.UUID;
 
 public class Person {
     private final UUID id;
+    @NotNull
     private final String name;
 
-    public Person(UUID id, String name) {
+    public Person(@JsonProperty("id") UUID id,
+
+                  @JsonProperty("name") String name) {
         this.id = id;
         this.name = name;
     }
